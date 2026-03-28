8dbc4f2de0ef21fae9c22bfdfda366a191de78a5
DikiYLexa <dubovan36@gmail.com>
2026-03-28 14:57:35 +0500

    Добавьте файлы проекта.

---
+++ Kino_Markov/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Kino_Markov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
